468e3cae85da2e55d5f77072202840500f903e59
poseidon-kimu <kaitofukin@yahoo.co.jp>
2018-08-18 01:27:09 +0900

    テスト１

---
+++ test1.php
@@ -0,0 +1,12 @@
+<?php require '../header.php'; ?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaito
+ * Date: 2018/08/18
+ * Time: 1:25
+ */
+echo "テスト";
+?>
+<?php require '../footer.php'; ?>
